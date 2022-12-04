8f40ac51b94b409128ad6801e5dde6330700accc
Hussein Ait-Lahcen <hussein.aitlahcen@gmail.com>
2022-12-04 12:56:36 +0100

    refactor and day3

--- Main.hs
+++ Main.hs
@@ -4,48 +4,53 @@
 module Main where
 
 import qualified Data.Attoparsec.ByteString.Char8 as A
-import Data.Bitraversable (Bitraversable (bitraverse))
 import qualified Data.ByteString.Char8 as BS
-import Data.Foldable (traverse_)
+import Data.Bitraversable (Bitraversable(..))
+import Data.Foldable (traverse_, foldMap')
 import Data.List (sort)
 import Data.Monoid (Sum (..))
 import Data.Ord (Down (..))
 import Data.Semigroup (Max (..))
+import Control.Applicative (Alternative(..))
+import Debug.Trace (traceShowId)
+import qualified Data.IntSet as S
+import Data.Char (isUpper, ord)
+import Data.Bifunctor.Join (Join(..))
 
 type Day = BS.ByteString -> IO ()
 
-doParse :: Either String c -> c
-doParse = either error id
+parseOrFail :: A.Parser a -> BS.ByteString -> a
+parseOrFail x = either error id . A.parseOnly x
 
 main :: IO ()
 main = traverse_ (\(f, i) -> f =<< BS.readFile ("day" <> show i <> ".txt")) $ zip days [1 ..]
   where
     days :: [Day]
-    days = [day1, day2]
+    days = [day1, day2, day3]
 
 day1 :: Day
 day1 = print . (\x -> (maxOfSum x, topThreeSum x)) . parse
   where
     parse =
-      doParse
-        . A.parseOnly
-          (A.sepBy' (A.sepBy' ((round @_ @Int) <$> A.scientific) A.space) A.space)
+      parseOrFail $ 
+          A.sepBy' (A.sepBy' ((round @_ @Int) <$> A.scientific) A.space) A.space
 
     maxOfSum =
       getMax
-        . foldMap (Max . getSum . foldMap Sum)
+        . foldMap' (Max . getSum . foldMap' Sum)
 
     topThreeSum =
       getSum
-        . foldMap (Sum . getDown)
+        . foldMap' getDown
         . take 3
         . sort
-        . fmap (Down . getSum . foldMap Sum)
+        . fmap (Down . foldMap' Sum)
 
 data Day2Shape
   = Rock
   | Paper
   | Scissor
+  deriving Show
 
 day2ShapeScore :: Day2Shape -> Int
 day2ShapeScore Rock = 1
@@ -56,6 +61,7 @@ data Day2RoundOutcome
   = Draw
   | Win
   | Lose
+  deriving Show
 
 day2Score :: Day2RoundOutcome -> Int
 day2Score Lose = 0
@@ -85,38 +91,83 @@ day2ShapeRecover x Draw = x
 day2 :: Day
 day2 = print . sequence [totalScore . parse, totalScoreRecovered . parseRecover]
   where
-    parseRPS 'A' = Rock
-    parseRPS 'B' = Paper
-    parseRPS 'C' = Scissor
-    parseRPS 'X' = Rock
-    parseRPS 'Y' = Paper
-    parseRPS 'Z' = Scissor
-    parseRPS _ = error "impossible"
-    parseRoundShape = parseRPS <$> A.satisfy (`elem` ['A', 'B', 'C', 'X', 'Y', 'Z'])
+    parseEntry c e = A.char c *> pure e
+    oponnentRock = parseEntry 'A' Rock
+    oponnentPaper = parseEntry 'B' Paper
+    oponnentScissor = parseEntry 'C' Scissor
 
-    parseRO 'X' = Lose
-    parseRO 'Y' = Draw
-    parseRO 'Z' = Win
-    parseRO _ = error "impossible"
-    parseRoundOutcome = parseRO <$> A.satisfy (`elem` ['X', 'Y', 'Z'])
+    rock = parseEntry 'X' Rock
+    paper = parseEntry 'Y' Paper
+    scissor = parseEntry 'Z' Scissor
+
+    parseRoundShape = 
+      oponnentRock 
+        <|> oponnentPaper 
+        <|> oponnentScissor 
+        <|> rock 
+        <|>  paper 
+        <|> scissor
+
+    lose = parseEntry 'X' Lose
+    draw = parseEntry 'Y' Draw
+    win = parseEntry 'Z' Win
+
+    parseRoundOutcome = lose <|> draw <|> win
 
     parse =
-      doParse
-        . A.parseOnly
-          (A.sepBy' ((,) <$> parseRoundShape <*> (A.space *> parseRoundShape)) A.space)
+      parseOrFail $
+          A.sepBy' ((,) <$> parseRoundShape <*> (A.space *> parseRoundShape)) A.space
 
     parseRecover =
-      doParse
-        . A.parseOnly
-          (A.sepBy' ((,) <$> parseRoundShape <*> (A.space *> parseRoundOutcome)) A.space)
+      parseOrFail $
+          A.sepBy' ((,) <$> parseRoundShape <*> (A.space *> parseRoundOutcome)) A.space
 
     computeScore (oponnentShape, shape) =
       day2Score (day2Round oponnentShape shape) + day2ShapeScore shape
 
-    totalScore = getSum . foldMap (Sum . computeScore)
+    totalScore = getSum . foldMap' (Sum . computeScore)
 
     computeScoreRecover (oponnentShape, outcome) =
       day2Score outcome + day2ShapeScore (day2ShapeRecover oponnentShape outcome)
 
     totalScoreRecovered =
-      getSum . foldMap (Sum . computeScoreRecover)
+      getSum . foldMap' (Sum . computeScoreRecover)
+
+
+day3 :: Day
+day3 = print . sequence [sumOfAllCommon . parseBags, sumOfAllCommonInGroups . parseGroupBags]
+  where
+    parseBag = A.many' A.letter_ascii
+
+    parseBags = 
+      parseOrFail $ A.sepBy' parseBag A.space
+
+    parseGroupBags =
+      parseOrFail $
+        A.sepBy'
+          ((,,) 
+            <$> parseBag 
+            <*> (A.space *> parseBag) 
+            <*> (A.space *> parseBag)) 
+          A.space
+
+    priority x 
+      | isUpper x = ord x - ord 'A' + 27
+      | otherwise = ord x - ord 'a' + 1
+
+    transform = S.fromList . fmap priority
+
+    compartify x = 
+      runJoin . fmap transform . Join $ splitAt (length x `div` 2) x
+
+    commonInBag = uncurry S.intersection . compartify
+
+    sumOfCommon = getSum . foldMap' Sum . S.toList
+
+    sumOfAllCommon = getSum . foldMap' (Sum . sumOfCommon . commonInBag)
+
+    commonInGroup (x, y, z) =
+       transform x `S.intersection` transform y `S.intersection` transform z
+
+    sumOfAllCommonInGroups = 
+      getSum . foldMap' (foldMap' Sum . S.toList . commonInGroup)
